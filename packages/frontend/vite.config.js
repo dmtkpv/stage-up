90501ef3be878c82893edfa2dad85e5af6f12373
Dmitriy Karpov <dmtkpv@gmail.com>
2025-09-19 09:30:42 +0300

    frontend: refactor vite.config.js

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue'
 import svg from 'vite-svg-loader'
-import { defineConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 
@@ -25,46 +24,38 @@ const {
 // Exports
 // -------------------
 
-export default defineConfig(({ ssrBuild }) => {
+export default {
 
-    return {
+    plugins: [
+        vue(),
+        svg({ svgo: false }),
+        // visualizer({ template: 'treemap', open: true })
+    ],
 
-        build: {
-            // sourcemap: 'hidden'
-        },
+    define: {
+        APP_VERSION: JSON.stringify(VERSION),
+        FRONTEND_URL: JSON.stringify(FRONTEND_URL),
+        BACKEND_URL: JSON.stringify(BACKEND_URL),
+        MAPS_KEY: JSON.stringify(MAPS_KEY),
+        MAP_ID: JSON.stringify(MAP_ID),
+        CRISP_WEBSITE_ID: JSON.stringify(CRISP_WEBSITE_ID)
+    },
 
-        plugins: [
-            vue(),
-            svg({ svgo: false }),
-            // visualizer({ template: 'treemap', open: true })
-        ],
-
-        define: {
-            APP_VERSION: JSON.stringify(VERSION),
-            FRONTEND_URL: JSON.stringify(FRONTEND_URL),
-            BACKEND_URL: JSON.stringify(BACKEND_URL),
-            MAPS_KEY: JSON.stringify(MAPS_KEY),
-            MAP_ID: JSON.stringify(MAP_ID),
-            CRISP_WEBSITE_ID: JSON.stringify(CRISP_WEBSITE_ID)
-        },
-
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
                     @use "sass:math";
                     @import "#root/styles/abstract.scss";
                 `,
-                }
-            }
-        },
-
-        server: {
-            hmr: {
-                port: FRONTEND_HMR_PORT
             }
         }
+    },
 
+    server: {
+        hmr: {
+            port: FRONTEND_HMR_PORT
+        }
     }
 
-});
+}
